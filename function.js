fe6a2fbc76843e88c9041631719243f18e4d7970
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2021-11-25 22:35:56 +0100

    learninf functions

---
+++ function.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let x = 10;
+// if (true) {
+// let y = 20;
+// var z = 30;
+// console.log(x + y + z);
+// // → 60
+// }
+// y is not visible here
+console.log(x + z);
+// console.log(y);
+// → 40
+
+
+
+
+// const square =(x)=>{
+//     return x*x;
+// }
+// console.log(square(4))
